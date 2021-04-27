f099fc91ec14429572c52fbe265f006042079d4f
Andrew <kevra@bk.ru>
2021-04-27 16:16:23 +0300

    Слайды Урок 62 я списал код так как урок сломан

--- script.js
+++ script.js
@@ -560,50 +560,77 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // // Slider
 
-    // let slideIndex = 1;
-    // const slides = document.querySelectorAll('.offer__slide'),
-    //     prev = document.querySelector('.offer__slider-prev'),
-    //     next = document.querySelector('.offer__slider-next'),
-    //     total = document.querySelector('#total'),
-    //     current = document.querySelector('#current');
+    let offset = 0;
+    let slideIndex = 1;
 
-    // showSlides(slideIndex);
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slidesField = document.querySelector('.offer__slider-inner');
 
-    // if (slides.length < 10) {
-    //     total.textContent = `0${slides.length}`;
-    // } else {
-    //     total.textContent = slides.length;
-    // }
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent =  `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent =  slideIndex;
+    }
+    
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
 
-    // function showSlides(n) {
-    //     if (n > slides.length) {
-    //         slideIndex = 1;
-    //     }
-    //     if (n < 1) {
-    //         slideIndex = slides.length;
-    //     }
+    slidesWrapper.style.overflow = 'hidden';
 
-    //     slides.forEach((item) => item.style.display = 'none');
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
 
-    //     slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
-        
-    //     if (slides.length < 10) {
-    //         current.textContent =  `0${slideIndex}`;
-    //     } else {
-    //         current.textContent =  slideIndex;
-    //     }
-    // }
+    next.addEventListener('click', () => {
+        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2); 
+        }
 
-    // function plusSlides (n) {
-    //     showSlides(slideIndex += n);
-    // }
+        slidesField.style.transform = `translateX(-${offset}px)`;
 
-    // prev.addEventListener('click', function(){
-    //     plusSlides(-1);
-    // });
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
 
-    // next.addEventListener('click', function(){
-    //     plusSlides(1);
-    // });
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
 });
-
